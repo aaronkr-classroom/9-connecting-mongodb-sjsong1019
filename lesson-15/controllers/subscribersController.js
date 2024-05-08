0f959d09a1abd21a3eb07acc8153f6a1a7b91f3d
sjsong1019 <163220368+sjsong1019@users.noreply.github.com>
2024-05-08 02:23:56 +0000

    9주차 끝!

--- lesson-15/controllers/subscribersController.js
+++ lesson-15/controllers/subscribersController.js
@@ -10,7 +10,7 @@ const Subscriber = require("../models/subscriber");
  */
 // OLD: 데이터베이스로부터의 데이터를 다음 미들웨어 함수로 전달하기 위해
 exports.getAllSubscribers = (req, res, next) => {
-  Subscriber.find({}, (error, subscribers) => {
+  Subscriber.find({}), (error, subscribers) => {
     // 구독자 모델에서 검색 쿼리
     if (error) next(error); // 에러를 미들웨어 함수로 전달
     req.data = subscribers; // 요청 객체에 대해 몽고DB로부터 돌아온 데이터의 세팅
@@ -20,7 +20,7 @@ exports.getAllSubscribers = (req, res, next) => {
     res.render("subscribers", {
       subscribers: subscribers,
     });
-  });
+  };
 };
 
 /**
